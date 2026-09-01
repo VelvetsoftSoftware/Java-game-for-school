95a158cbbe549bc3ea57f35c657c3db3777f2ad7
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-08-31 23:55:31 -0400

    Add files via upload

---
+++ Textures.java
@@ -0,0 +1,16 @@
+public class Textures {
+	//this is were all textures are stored in ROM
+	    public static final byte[][] TEXTURES = {
+        {
+            // texture 0
+        },
+        {
+            // texture 1
+        },
+        {
+            // texture 2
+        }
+    };
+	
+	
+}

8f71e51f0c2a81b5f93e2195d260ded694e8c239
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-09-03 23:10:32 -0400

    Add files via upload

---
+++ windowGenerator.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class window {
+	JFrame frame = new JFrame("Test Window");
+	
+	frame.setSize(320, 240);
+	frame.setDefaultCloseOperation(JFreame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	
+}

2b4be2ac1389e6040afaa990e0dd1d42c82ce407
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-09-11 20:34:23 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,24 @@
 public class main {
+	//dont forget to set firstrun to 0 before the tranistion of the level
+	public static int gameState = 0, firstrun = 0;
+	
 	public static void main(String[] args) {
 		Window window = new Window();
-
-		intro.loadintro(window);
 		
 		while(true) {
-			try {
-				Thread.sleep(16);
-			} catch (InterruptedException e) {
-				break;
-			}	
+			switch(gameState) {
+				case 0:
+					intro.loadintro(window);
+					break;
+				case 1:
+					mainmenu.drawmainmenu(window);
+					break;
+			}
+					
+			window.clearBuffers();
+			utility.sleep60fps();
+			for (int i = 0; i < 256; i++)
+				input.pressed[i] = false;	
 		}
 	}
 }
